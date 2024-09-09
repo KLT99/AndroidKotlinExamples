fb4be16a5140a161191c581d908242f4f735c09b
KLT <kari9480@gmail.com>
2024-09-09 11:28:28 -0600

    Se creo nueva Activity con su .kt y .xml

---
+++ app/src/main/java/com/klt/androidkotlinexamples/supeheroapp/SuperHeroListActivity.kt
@@ -0,0 +1,21 @@
+package com.klt.androidkotlinexamples.supeheroapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.klt.androidkotlinexamples.R
+
+class SuperHeroListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_super_hero_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
