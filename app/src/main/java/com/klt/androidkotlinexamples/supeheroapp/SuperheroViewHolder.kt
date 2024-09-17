c110b4a284776bc597b4f2d216147fb036f98ffb
KLT <kari9480@gmail.com>
2024-09-17 13:25:27 -0600

    Se añadio funcion setOnclickListener para que al momento de que seleccione un item mande el id del superhero correspondiente

--- app/src/main/java/com/klt/androidkotlinexamples/supeheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/klt/androidkotlinexamples/supeheroapp/SuperheroViewHolder.kt
@@ -9,11 +9,15 @@ class SuperheroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemSuperheroBinding.bind(view)
 
-    fun bind(superHeroItemResponse: SuperHeroItemResponse){
+    fun bind(superHeroItemResponse: SuperHeroItemResponse, onItemSelected: (String) -> Unit) {
 
         binding.tvSuperheroName.text = superHeroItemResponse.name
 
         Picasso.get().load(superHeroItemResponse.superheroImage.url).into(binding.ivSuperhero)
+
+        binding.root.setOnClickListener {
+            onItemSelected(superHeroItemResponse.superheroId)
+        }
     }
 
 }
