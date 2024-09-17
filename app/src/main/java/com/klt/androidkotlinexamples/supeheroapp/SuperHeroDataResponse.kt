f75b41776e898d99ca85ab0296ef1f73598255e2
KLT <kari9480@gmail.com>
2024-09-17 13:00:04 -0600

    se añadio el objeto de image (url) para poder llamar la imagen

--- app/src/main/java/com/klt/androidkotlinexamples/supeheroapp/SuperHeroDataResponse.kt
+++ app/src/main/java/com/klt/androidkotlinexamples/supeheroapp/SuperHeroDataResponse.kt
@@ -9,5 +9,8 @@ data class SuperHeroDataResponse(
 
 data class SuperHeroItemResponse(
     @SerializedName("id") val superheroId: String,
-    @SerializedName("name") val name: String
+    @SerializedName("name") val name: String,
+    @SerializedName("image") val superheroImage: SuperheroImageResponse
 )
+
+data class SuperheroImageResponse(@SerializedName("url") val url: String)
