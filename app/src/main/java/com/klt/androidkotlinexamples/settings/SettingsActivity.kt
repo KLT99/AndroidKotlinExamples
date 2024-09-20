6d79313554ac7dd5b8dc300cc7557bf9d7f6ff9c
KLT <kari9480@gmail.com>
2024-09-20 14:36:05 -0600

    Se creo una nueva clase llamada SettingsActivity.kt

---
+++ app/src/main/java/com/klt/androidkotlinexamples/settings/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.klt.androidkotlinexamples.settings
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.klt.androidkotlinexamples.R
+import com.klt.androidkotlinexamples.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+    }
+}
